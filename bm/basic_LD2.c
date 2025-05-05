7d38f86b83185fa00d41b9a8c1e3b151fa39ec59
Gabriel Vasquez <83047297+gabofv@users.noreply.github.com>
2025-05-05 17:53:08 -0600

    Remove default comments

--- bm/basic_LD2.c
+++ bm/basic_LD2.c
@@ -4,16 +4,6 @@
  * @author         : Gabriel Vasquez
  * @brief          : Turn ON LD2 on the board
  ******************************************************************************
- * @attention
- *
- * Copyright (c) 2025 STMicroelectronics.
- * All rights reserved.
- *
- * This software is licensed under terms that can be found in the LICENSE file
- * in the root directory of this software component.
- * If no LICENSE file comes with this software, it is provided AS-IS.
- *
- ******************************************************************************
  */
 
 #include <stdint.h>
