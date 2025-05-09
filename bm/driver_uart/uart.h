b75680d8de7efd907067bf3fc17893835135ec02
Gabriel Vasquez <83047297+gabofv@users.noreply.github.com>
2025-05-08 18:56:49 -0600

    Create uart.h

---
+++ bm/driver_uart/uart.h
@@ -0,0 +1,17 @@
+/*
+ *************************************************************************************
+ * @file           : uart.h
+ * @author         : Gabriel Vasquez
+ * @brief          : Header file for UART driver (USART2)
+ *************************************************************************************
+ */
+
+#ifndef UART_H_
+#define UART_H_
+
+#include <stm32l4xx.h>
+#include <stdint.h>
+
+void uart_init(void);
+
+#endif /* UART_H_ */
